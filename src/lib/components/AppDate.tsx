016c8b1d3ad5591cdab7d3260f19a9bce99672c8
Aurel <aurelticot.dev@gmail.com>
2020-10-25 07:57:24 +1100

    display main date as relative

--- src/lib/components/AppDate.tsx
+++ src/lib/components/AppDate.tsx
@@ -1,20 +1,49 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { useIntl } from "react-intl";
+import { useRealTime } from "../hooks";
+import { DateTime } from "luxon";
 
 interface AppDateProps {
   time: Date | null;
 }
 
-export const AppDate = ({ time }: AppDateProps): JSX.Element => {
+export const AppDate: React.FC<AppDateProps> = ({ time }) => {
+  const realtime = useRealTime();
   const i18n = useIntl();
 
-  //i18n.formatMessage({ id: "AppDateToday", defaultMessage: "Today" })
+  const labelToday = i18n.formatMessage({
+    id: "AppDate.today",
+    defaultMessage: "Today",
+  });
 
-  return (
-    <>
-      {time && <Box>{i18n.formatDate(time)}</Box>}
-      {!time && <Box>{i18n.formatDate(new Date())}</Box>}
-    </>
-  );
+  if (!time) {
+    return <Box>{labelToday}</Box>;
+  }
+
+  const realDateTime = DateTime.fromJSDate(realtime);
+  const dateTime = DateTime.fromJSDate(time);
+
+  if (dateTime.hasSame(realDateTime, "day")) {
+    return <Box>{labelToday}</Box>;
+  }
+
+  const labelTomorrow = i18n.formatMessage({
+    id: "AppDate.tomorrow",
+    defaultMessage: "Tomorrow",
+  });
+  const labelYesterday = i18n.formatMessage({
+    id: "AppDate.yesterday",
+    defaultMessage: "Yesterday",
+  });
+
+  if (dateTime.hasSame(realDateTime.plus({ day: 1 }), "day")) {
+    return <Box>{labelTomorrow}</Box>;
+  }
+
+  if (dateTime.hasSame(realDateTime.minus({ day: 1 }), "day")) {
+    return <Box>{labelYesterday}</Box>;
+  }
+
+  return <Box>{i18n.formatDate(time)}</Box>;
 };
