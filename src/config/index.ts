5e6f8d6440777d42d9cc41cb8573b701602532e2
Aurel <aurelticot.dev@gmail.com>
2020-10-30 22:21:38 +1100

    update config with properties from env vars

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,24 @@
 import { SettingKey, UserSettings, MarketSortingMethod } from "../lib/types";
 import { settings } from "./settings";
 
+const appName = process.env.npm_package_name;
+
+const appVersion = process.env.npm_package_version;
+
+const nodeEnv: string = process.env.NODE_ENV || "production";
+
+const environmentId: string = process.env.REACT_APP_ENVIRONMENT_ID || "unknown";
+
+const graphqlAPIEndpoint = process.env.REACT_APP_GRAPHQL_API_ENDPOINT;
+
+const raygunAPIKey = process.env.REACT_APP_RAYGUN_API_KEY;
+
+const enableMonitoring = !!(process.env.REACT_APP_ENABLE_MONITORING === "true");
+
+const enableCrashReporting = !!(
+  process.env.REACT_APP_ENABLE_CRASH_REPORTING === "true"
+);
+
 const timelineVisiblePeriod = 24;
 const daysInPast = 2;
 const daysRequestedInPast = daysInPast + 1;
@@ -15,6 +33,11 @@ const defaultUserSettings: UserSettings = {
 };
 
 export const config = {
+  appName,
+  appVersion,
+  nodeEnv,
+  environmentId,
+  graphqlAPIEndpoint,
   timelineVisiblePeriod,
   daysInPast,
   daysRequestedInPast,
@@ -22,4 +45,7 @@ export const config = {
   daysRequestedInFuture,
   settings,
   defaultUserSettings,
+  raygunAPIKey,
+  enableMonitoring,
+  enableCrashReporting,
 };
