5abc476040f29949f4acbc0bb5c2381003683d04
Aurel <aurelticot.dev@gmail.com>
2020-11-11 21:25:03 +1100

    open settings and market selection in a modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import { initRaygun } from "lib/utils";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { AppContextProvider } from "lib/contexts";
-import { CssBaseline } from "@material-ui/core";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Button,
+  CssBaseline,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import {
@@ -13,6 +28,7 @@ import {
   TimelineView,
 } from "sections";
 import * as serviceWorker from "./serviceWorker";
+import { useIntl } from "react-intl";
 
 initRaygun();
 
@@ -21,32 +37,90 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
   },
   appContainer: theme.mixins.toolbar,
+  dialogContent: {
+    padding: theme.spacing(1),
+  },
+  dialogContainer: {
+    backgroundColor: theme.palette.background.default,
+  },
 }));
 
 const App: React.FunctionComponent<{}> = () => {
-  const classes = useStyles();
+  const history = useHistory();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const settingsRouteMatch = useRouteMatch("/settings");
+  const marketSelectionRouteMatch = useRouteMatch("/selection");
+  const theme = useTheme();
+  const fullscreenDialog = useMediaQuery(theme.breakpoints.down("xs"));
+  const i18n = useIntl();
 
+  const closeDialog = () => {
+    history.goBack();
+    setDialogOpen(false);
+  };
+
+  useEffect(() => {
+    setDialogOpen(
+      !!settingsRouteMatch?.isExact || !!marketSelectionRouteMatch?.isExact
+    );
+  }, [settingsRouteMatch, marketSelectionRouteMatch]);
+
+  const modalCloseButtonLabel = i18n.formatMessage({
+    id: "App.modalDialog.closeButtonLabel",
+    description: "Label of the the 'Close' button in the main dialog",
+    defaultMessage: "Close",
+  });
+
+  const settingsModalTitle = i18n.formatMessage({
+    id: "App.modalDialog.settingsTitle",
+    description: "Title of the Settings dialog",
+    defaultMessage: "Settings",
+  });
+
+  const marketSelectionModalTitle = i18n.formatMessage({
+    id: "App.modalDialog.marketSelectionTitle",
+    description: "Title of the Market Selection dialog",
+    defaultMessage: "Market Selection",
+  });
+
+  const classes = useStyles();
   return (
-    <Router>
-      <Box className={classes.root}>
-        <ApplicationBar />
-        <Box className={classes.appContainer} />
-        <Switch>
-          <Route path="/" exact>
-            <TimelineView />
-          </Route>
-          <Route path="/timeline">
-            <TimelineView />
-          </Route>
-          <Route path="/settings">
-            <SettingsView />
-          </Route>
-          <Route path="/selection">
-            <MarketSelectionView />
-          </Route>
-        </Switch>
-      </Box>
-    </Router>
+    <Box className={classes.root}>
+      <ApplicationBar />
+      <Box className={classes.appContainer} />
+      <Switch>
+        <Route path={["/", "/settings", "/selection"]} exact>
+          <TimelineView />
+        </Route>
+      </Switch>
+      <Dialog
+        open={dialogOpen}
+        onClose={closeDialog}
+        fullWidth={true}
+        maxWidth="sm"
+        scroll="paper"
+        fullScreen={fullscreenDialog}
+        classes={
+          fullscreenDialog
+            ? {
+                paper: classes.dialogContainer,
+              }
+            : {}
+        }
+      >
+        <DialogTitle>
+          {!!settingsRouteMatch?.isExact && settingsModalTitle}
+          {!!marketSelectionRouteMatch?.isExact && marketSelectionModalTitle}
+        </DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          {!!settingsRouteMatch?.isExact && <SettingsView />}
+          {!!marketSelectionRouteMatch?.isExact && <MarketSelectionView />}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>{modalCloseButtonLabel}</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 };
 
@@ -54,7 +128,9 @@ ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
       <CssBaseline></CssBaseline>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AppContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
