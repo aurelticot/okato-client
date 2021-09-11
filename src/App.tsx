c68d7ce83c83b8490d2633de49404ab9b776742a
aurelticot <aurelticot.dev@gmail.com>
2021-09-11 12:48:25 +1000

    split index and App

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { sendPageView } from "lib/utils";
+import { routes } from "lib/constants";
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box } from "@material-ui/core";
+import {
+  ApplicationBar,
+  TimelineView,
+  MarketSelectionDialog,
+  SettingsDialog,
+} from "sections";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: `0 ${theme.spacing(1)}px`,
+  },
+  appContainer: theme.mixins.toolbar,
+  dialogContent: {
+    padding: theme.spacing(1),
+  },
+  dialogContainer: {
+    backgroundColor: theme.palette.background.default,
+  },
+}));
+
+export const App: React.FunctionComponent = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const settingsRouteMatch = useRouteMatch(routes.settings);
+  const marketSelectionRouteMatch = useRouteMatch(routes.marketSelection);
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+    history.push(routes.home);
+  };
+
+  useEffect(() => {
+    sendPageView(location.pathname);
+  }, [location]);
+
+  useEffect(() => {
+    setDialogOpen(
+      !!settingsRouteMatch?.isExact || !!marketSelectionRouteMatch?.isExact
+    );
+  }, [settingsRouteMatch, marketSelectionRouteMatch]);
+
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <ApplicationBar />
+      <Box className={classes.appContainer} />
+      <Switch>
+        <Route
+          path={[routes.home, routes.settings, routes.marketSelection]}
+          exact
+        >
+          <TimelineView />
+        </Route>
+      </Switch>
+      <MarketSelectionDialog
+        open={dialogOpen && !!marketSelectionRouteMatch?.isExact}
+        onClose={closeDialog}
+      />
+      <SettingsDialog
+        open={dialogOpen && !!settingsRouteMatch?.isExact}
+        onClose={closeDialog}
+      />
+    </Box>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,93 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { initTelemetry, sendPageView } from "lib/utils";
+import { initTelemetry } from "lib/utils";
 import { AppContextProvider } from "lib/contexts";
-import { routes } from "lib/constants";
 import { CssBaseline } from "@material-ui/core";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useRouteMatch,
-  useHistory,
-  useLocation,
-} from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box } from "@material-ui/core";
-import {
-  ApplicationBar,
-  TimelineView,
-  MarketSelectionDialog,
-  SettingsDialog,
-} from "sections";
+import { BrowserRouter as Router } from "react-router-dom";
+import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 initTelemetry();
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: `0 ${theme.spacing(1)}px`,
-  },
-  appContainer: theme.mixins.toolbar,
-  dialogContent: {
-    padding: theme.spacing(1),
-  },
-  dialogContainer: {
-    backgroundColor: theme.palette.background.default,
-  },
-}));
-
-const App: React.FunctionComponent = () => {
-  const history = useHistory();
-  const location = useLocation();
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const settingsRouteMatch = useRouteMatch(routes.settings);
-  const marketSelectionRouteMatch = useRouteMatch(routes.marketSelection);
-
-  const closeDialog = () => {
-    setDialogOpen(false);
-    history.push(routes.home);
-  };
-
-  useEffect(() => {
-    sendPageView(location.pathname);
-  }, [location]);
-
-  useEffect(() => {
-    setDialogOpen(
-      !!settingsRouteMatch?.isExact || !!marketSelectionRouteMatch?.isExact
-    );
-  }, [settingsRouteMatch, marketSelectionRouteMatch]);
-
-  const classes = useStyles();
-  return (
-    <Box className={classes.root}>
-      <ApplicationBar />
-      <Box className={classes.appContainer} />
-      <Switch>
-        <Route
-          path={[routes.home, routes.settings, routes.marketSelection]}
-          exact
-        >
-          <TimelineView />
-        </Route>
-      </Switch>
-      <MarketSelectionDialog
-        open={dialogOpen && !!marketSelectionRouteMatch?.isExact}
-        onClose={closeDialog}
-      />
-      <SettingsDialog
-        open={dialogOpen && !!settingsRouteMatch?.isExact}
-        onClose={closeDialog}
-      />
-    </Box>
-  );
-};
-
 ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
-      <CssBaseline></CssBaseline>
+      <CssBaseline />
       <Router>
         <App />
       </Router>
